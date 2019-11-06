8cc7f937ef5b811f05ec82c4e5d5927ee0eec755
Carlos Martínez <ingeniero.carlos.martinez@gmail.com>
2019-11-06 17:49:56 +0100

    - Adaptations

--- src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
@@ -16,34 +16,25 @@
  */
 package guru.sfg.beer.inventory.service.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import java.sql.Timestamp;
 import java.util.UUID;
 
-/**
- * Created by jt on 2019-01-26.
- */
+/** Created by jt on 2019-01-26. */
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-public class BeerInventory extends BaseEntity{
+public class BeerInventory extends BaseEntity {
 
-    @Builder
-    public BeerInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, UUID beerId,
-                         String upc, Integer quantityOnHand) {
-        super(id, version, createdDate, lastModifiedDate);
-        this.beerId = beerId;
-        this.upc = upc;
-        this.quantityOnHand = quantityOnHand;
-    }
-
-    private UUID beerId;
-    private String upc;
-    private Integer quantityOnHand = 0;
+  private UUID beerId;
+  private String upc;
+  private Integer quantityOnHand = 0;
 }
